4d234bb6d2f97f99851773280fb8c74f752ae781
Victor Roma <66295991+RomaNFS21@users.noreply.github.com>
2024-11-19 20:36:53 -0300

    Adicionado comentarios nos programas 1 e 2

--- Programa01.js
+++ Programa01.js
@@ -1,9 +1,13 @@
+// Declaração do vetor
 var numbers = [10, 20, 30, 40, 50]
 
+// Adiciona o valor 60 ao final do vetor
 numbers.push(60)
 console.log(numbers)
 
+// Retira o ultimo valor do vetor
 numbers.pop()
 console.log(numbers)
 
+// Mostra a posição do valor 30
 console.log(numbers.indexOf(30))
--- Programa02.js
+++ Programa02.js
@@ -1,5 +1,7 @@
+// Declaração de variveis
 let temperature = 50;
 
+// Verificações das condições e impressão do nivel de calor
 if (temperature > 30 && temperature <= 40){
     console.log("Está Quente!");
 }
