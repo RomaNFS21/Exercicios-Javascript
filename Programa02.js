5fbac970837238cb22917082edd037e7b9dece6e
Victor Roma <66295991+RomaNFS21@users.noreply.github.com>
2024-11-19 19:59:10 -0300

    Criado e finalizado Programa 2

---
+++ Programa02.js
@@ -0,0 +1,17 @@
+let temperature = 50;
+
+if (temperature > 30 && temperature <= 40){
+    console.log("Está Quente!");
+}
+else if (temperature > 40){
+        console.log("Está Muito Quente!");
+}
+
+else{
+    console.log("Está Frio!");
+}
+
+
+
+    
+
